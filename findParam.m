c603eea897e7004d01f48ab77a40210bced9a476
Matthieu Raymond <mpr15@ray35.doc.ic.ac.uk>
2015-12-02 13:52:31 +0000

    Change find_param to perform misclassification rate calculation on val set

--- findParam.m
+++ findParam.m
@@ -12,7 +12,7 @@ res = zeros(100,3);
 indexer = 1;
 
 for layers = 1:5
-    for neurons_per_layers = (1:4)*10
+    for neurons_per_layers = [10 20 30 50 100]
         
         l = ones(1,layers) * neurons_per_layers;
         errSum = 0;
@@ -34,13 +34,26 @@ for layers = 1:5
 
             [N, tr] = createNetwork(a, b, l);
             
-            res(indexer, 1) = layers;
-            res(indexer, 2) = neurons_per_layers;
-            res(indexer, 3) = tr.best_vperf;
-            indexer = indexer + 1;
+            nbError = 0;
+            
+            for i = 1 : (1 + ending - starting)
+                predicted = predict(N, testA(:, i));
+
+                if (1 == testB(predicted, i))
+                   nbError = nbError + 1;
+                end
+            end
+            
+            avg = nbError/(1 + ending - starting);
+            errSum = errSum + avg;           
             
             x = originalX;
             y = originalY;
         end
+        
+        res(indexer, 1) = layers;
+        res(indexer, 2) = neurons_per_layers;
+        res(indexer, 3) = 100*errSum/trainingSize;
+        indexer = indexer + 1;
     end
 end
